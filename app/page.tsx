c94861e235729a8ff7765abc128f4396238b6952
Jihoon LEE <jihoon.lee@lnrobo.com>
2024-12-09 16:18:57 +0900

    Modify landing page

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,9 @@ export default async function Index() {
     <>
       <Hero />
       <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
+        <p className="flex gap-8 justify-center items-center">
+          Medical Device Digital Development Tools
+        </p>
       </main>
     </>
   );
