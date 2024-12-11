886c9ada7489b2f324cb423129a01480fb9f892a
Jihoon LEE <jihoon.lee@lnrobo.com>
2024-12-11 14:05:31 +0900

    Modify routing
    - recover top-left link with title string
    - default routing: / -> /items
    - sign-out routing: /

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
-                    {/* left-side buttons on top bar */}
+                    <Link href={"/"}>{metadata.title}</Link>
                   </div>
                   {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
                 </div>
